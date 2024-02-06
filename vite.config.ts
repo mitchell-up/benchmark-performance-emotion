8cacbe463855ec65a7cbae9de554be97ab43d11c
rangex-mitchell <mitchellkim94@icloud.com>
2024-02-06 23:38:16 +0900

    chore: joyUI, Emotion, tanstack-router added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    plugins: [['@swc/plugin-emotion', {}]]
-  })],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      plugins: [['@swc/plugin-emotion', {}]]
+    }),
+   TanStackRouterVite()
+  ],
 })
