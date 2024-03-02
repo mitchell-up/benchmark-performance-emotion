7d1330ce9c58e058d8c1453f3576f9d6b84150ef
mitchell <mitchellkim94@icloud.com>
2024-03-02 17:17:21 +0900

    chore: build config modified

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+const profiling = isProduction && {
+  'react-dom/client': 'react-dom/profiling',
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,4 +18,9 @@ export default defineConfig({
     TanStackRouterVite(),
   ],
   base: '/benchmark-performance-emotion/',
+  resolve: {
+    alias: {
+      ...profiling,
+    },
+  },
 })
