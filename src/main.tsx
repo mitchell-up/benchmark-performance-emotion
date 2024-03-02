bb00894bb1ce4b2cb643d7f9df3c46836dafc1a5
mitchell <mitchellkim94@icloud.com>
2024-03-02 16:19:00 +0900

    fix: base url set to router

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,10 @@ import './reset.css'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  basepath: 'benchmark-performance-emotion',
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
