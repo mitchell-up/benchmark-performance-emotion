37cfbe21b92afe91c9aca600ebf3a38c805f6953
mitchell <mitchellkim94@icloud.com>
2024-05-03 18:35:00 +0900

    refactor: styled vs cssprop (object)

---
+++ src/components/subjects/css-object/ToggleStyled.tsx
@@ -0,0 +1,71 @@
+import styled from '@emotion/styled'
+import { Layout } from '../../subject/Layout'
+import { RENDER_COUNT } from './data'
+
+const StyledToggle = styled.label({
+  display: 'inline-flex',
+
+  "& input[type='checkbox']": {
+    cursor: 'pointer',
+    position: 'relative',
+    appearance: 'none',
+    width: '48px',
+    height: '28px',
+    backgroundColor: 'lightgray',
+    borderRadius: '16px',
+    transition: 'background-color 0.2s ease-in-out',
+    overflow: 'hidden',
+
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      width: '20px',
+      height: '20px',
+      backgroundColor: 'gray',
+      borderRadius: '50%',
+      left: '4px',
+      top: '4px',
+      marginBlock: 'auto',
+      transition: 'transform 0.2s ease',
+      boxShadow: '-2px 2px 10px 0px rgba(0, 0, 0, 0.35)',
+    },
+
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      top: '0',
+      right: '0',
+      bottom: '0',
+      left: '0',
+      backgroundColor: 'transparent',
+      transition: 'background-color 0.2s ease-in-out',
+    },
+
+    '&:active': {
+      '&::after': {
+        backgroundColor: 'white',
+      },
+    },
+  },
+
+  "& input[type='checkbox']:checked": {
+    backgroundColor: 'pink',
+
+    '&::before': {
+      backgroundColor: 'white',
+      transform: 'translateX(100%)',
+    },
+  },
+})
+
+export function TogglesStyled() {
+  return (
+    <Layout>
+      {Array.from({ length: RENDER_COUNT }).map((_, idx) => (
+        <StyledToggle key={idx}>
+          <input type='checkbox' />
+        </StyledToggle>
+      ))}
+    </Layout>
+  )
+}
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import PageLayout from '../components/PageLayout'
-import { CssToggles, StyledToggles } from '../components/subject/StyledObject'
+import { TogglesStyled } from '../components/subjects/css-object/ToggleStyled'
+import { TogglesCssProp } from '../components/subjects/css-object/ToggleCssProp'
 
 export const Route = createLazyFileRoute('/')({
   component: Styledcss,
@@ -11,9 +12,9 @@ function Styledcss() {
     <PageLayout
       title='Styled vs CssProp (Object)'
       description='Object 형식으로 스타일링 하는 상황에서 styled와 cssProp의 성능을 비교합니다.'
-      compA={<StyledToggles />}
+      compA={<TogglesStyled />}
       compAName='Styled'
-      compB={<CssToggles />}
+      compB={<TogglesCssProp />}
       compBName='CssProp'
     />
   )
