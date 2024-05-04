228fe827bf883950e70208de19cc128c1c5fc4e9
mitchell <mitchellkim94@icloud.com>
2024-05-04 19:33:12 +0900

    refactor: styledliteral

---
+++ src/components/subjects/styledliteral/TogglesCssProp.tsx
@@ -0,0 +1,71 @@
+import { css } from '@emotion/react'
+import { Layout } from '../../subject/Layout'
+import { RENDER_COUNT } from './data'
+
+const cssObj = css`
+  display: inline-flex;
+
+  & input[type='checkbox'] {
+    cursor: pointer;
+    position: relative;
+    appearance: none;
+    width: 48px;
+    height: 28px;
+    background-color: lightgray;
+    border-radius: 16px;
+    transition: background-color 0.2s ease-in-out;
+    overflow: hidden;
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      background-color: gray;
+      border-radius: 50%;
+      left: 4px;
+      top: 4px;
+      margin-block: auto;
+      transition: transform 0.2s ease;
+      box-shadow: -2px 2px 10px 0px rgba(0, 0, 0, 0.35);
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background-color: transparent;
+      transition: background-color 0.2s ease-in-out;
+    }
+
+    &:active {
+      &::after {
+        background-color: white;
+      }
+    }
+  }
+
+  & input[type='checkbox']:checked {
+    background-color: pink;
+
+    &::before {
+      background-color: white;
+      transform: translateX(100%);
+    }
+  }
+`
+
+export function TogglesCssProp() {
+  return (
+    <Layout>
+      {Array.from({ length: RENDER_COUNT }).map((_, idx) => (
+        <label css={cssObj} key={idx}>
+          <input type='checkbox' />
+        </label>
+      ))}
+    </Layout>
+  )
+}
--- src/routes/styledliteral.lazy.tsx
+++ src/routes/styledliteral.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import PageLayout from '../components/PageLayout'
-import { CssToggles, StyledToggles } from '../components/subject/StyledLiteral'
+import { TogglesStyled } from '../components/subjects/styledliteral/TogglesStyled'
+import { TogglesCssProp } from '../components/subjects/styledliteral/TogglesCssProp'
 
 export const Route = createLazyFileRoute('/styledliteral')({
   component: Component,
@@ -11,9 +12,9 @@ function Component() {
     <PageLayout
       title='Styled vs CssProp (Literal)'
       description='Template Literal 형식으로 스타일링 하는 상황에서 styled와 cssProp의 성능을 비교합니다.'
-      compA={<StyledToggles />}
+      compA={<TogglesStyled />}
       compAName='Styled'
-      compB={<CssToggles />}
+      compB={<TogglesCssProp />}
       compBName='CssProp'
     />
   )
